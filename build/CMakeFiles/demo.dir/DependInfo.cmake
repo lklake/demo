1862e6ba099f4cfe2b023dbfbfecc3460130e1b9
CSWYF3634076 <3634076@qq.com>
2023-04-03 11:12:18 +0800

    x and dbus v1

---
+++ build/CMakeFiles/demo.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/zhangxg/github/demo/LinkedList.c" "CMakeFiles/demo.dir/LinkedList.c.o" "gcc" "CMakeFiles/demo.dir/LinkedList.c.o.d"
+  "/home/zhangxg/github/demo/client.c" "CMakeFiles/demo.dir/client.c.o" "gcc" "CMakeFiles/demo.dir/client.c.o.d"
+  "/home/zhangxg/github/demo/kbd_client.c" "CMakeFiles/demo.dir/kbd_client.c.o" "gcc" "CMakeFiles/demo.dir/kbd_client.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
