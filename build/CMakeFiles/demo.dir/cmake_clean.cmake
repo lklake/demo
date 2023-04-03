ae6840b76cd7d3fa312eff40f769916ee7011e4b
CSWYF3634076 <3634076@qq.com>
2023-04-03 13:23:59 +0800

    x and dbus v1

---
+++ build/CMakeFiles/demo.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo.dir/LinkedList.c.o"
+  "CMakeFiles/demo.dir/LinkedList.c.o.d"
+  "CMakeFiles/demo.dir/client.c.o"
+  "CMakeFiles/demo.dir/client.c.o.d"
+  "CMakeFiles/demo.dir/kbd_client.c.o"
+  "CMakeFiles/demo.dir/kbd_client.c.o.d"
+  "demo"
+  "demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
